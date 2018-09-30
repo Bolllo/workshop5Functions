5203fe1d57d87f2722679d1a02780d6d77ca6ce3
ever0035 <ever0035@hz.nl>
2018-09-30 17:01:48 +0200

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+//task 1
+/*const fibonacci = function () {
+let f0 = 0;
+let f1 = 1;
+let fn = 0;
+
+while (f0 <= 150) {
+console.log(f1)
+fn = f0+f1;
+f0 = f1
+f1 = fn
+}
+return f1
+}
+fibonacci()
+*/
+//task 2
+const countdown = function (jaartal) {
+    let digit = 10;
+    for (digit = 10; digit >= 0; digit--){
+        alert(digit);
+        console.log(digit)
+    }
+    console.log(`happy ${jaartal}!`)
+} 
+countdown(2018)
+//task 3
